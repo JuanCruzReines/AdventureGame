fa94bec5a1555bd7109e6ca285c5d65938c19475
Juan Cruz Reines <juancruz.reines@gmail.com>
2017-02-23 17:56:09 -0300

    Modifico script del jugador para manejar interaccion con objetos

--- Assets/Scripts/MonoBehaviours/Player/PlayerMovement.cs
+++ Assets/Scripts/MonoBehaviours/Player/PlayerMovement.cs
@@ -17,6 +17,8 @@ public class PlayerMovement : MonoBehaviour {
 
     private WaitForSeconds inputHoldWait;
     private Vector3 destinationPosition;
+    private Interactable currentInteractable;
+    private bool handleInput = true;
 
 
     private const float stopDistanceProportion = 0.1f;
@@ -24,6 +26,7 @@ public class PlayerMovement : MonoBehaviour {
 
 
     private readonly int hashSpeedPara = Animator.StringToHash("Speed");
+    private readonly int hashLocomotionTag = Animator.StringToHash("Locomotion");
 
     private void Start()
     {
@@ -46,15 +49,15 @@ public class PlayerMovement : MonoBehaviour {
 
         float speed = agent.desiredVelocity.magnitude;
 
-        if(agent.remainingDistance <= agent.stoppingDistance * stopDistanceProportion)
+        if (agent.remainingDistance <= agent.stoppingDistance * stopDistanceProportion)
         {
             Stopping(out speed);
         }
-        else if(agent.remainingDistance <= agent.stoppingDistance)
+        else if (agent.remainingDistance <= agent.stoppingDistance)
         {
             Slowing(out speed, agent.remainingDistance);
         }
-        else if(speed > turnSpeedThreshold)
+        else if (speed > turnSpeedThreshold)
         {
             Moving();
         }
@@ -67,6 +70,15 @@ public class PlayerMovement : MonoBehaviour {
         agent.Stop();
         transform.position = destinationPosition;
         speed = 0f;
+
+        if (currentInteractable)
+        {
+            transform.rotation = currentInteractable.interactionLocation.rotation;
+            currentInteractable.Interact();
+            currentInteractable = null;
+
+            StartCoroutine(WaitForInteraction());
+        }
     }
 
     private void Slowing(out float speed, float distanceToDestination)
@@ -76,6 +88,9 @@ public class PlayerMovement : MonoBehaviour {
 
         float proportionalDistance = 1f - distanceToDestination / agent.stoppingDistance;
         speed = Mathf.Lerp(slowingSpeed, 0f, proportionalDistance);
+
+        Quaternion targetRotation = currentInteractable ? currentInteractable.interactionLocation.rotation : transform.rotation;
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, proportionalDistance);
     }
 
     private void Moving()
@@ -86,6 +101,12 @@ public class PlayerMovement : MonoBehaviour {
 
     public void OnGroundClick(BaseEventData data)
     {
+        if (!handleInput)
+            return;
+
+        currentInteractable = null;
+
+
         PointerEventData pData = (PointerEventData)data;
 
         NavMeshHit hit;
@@ -101,5 +122,30 @@ public class PlayerMovement : MonoBehaviour {
         agent.SetDestination(destinationPosition);
         agent.Resume();
 
+    }
+
+    public void OnInteractableClick(Interactable interactable)
+    {
+        if (!handleInput)
+            return;
+
+        currentInteractable = interactable;
+        destinationPosition = currentInteractable.interactionLocation.position;
+        agent.SetDestination(destinationPosition);
+        agent.Resume();
+    }
+
+    private IEnumerator WaitForInteraction()
+    {
+        handleInput = false;
+
+        yield return inputHoldWait;
+
+        while(animator.GetCurrentAnimatorStateInfo(0).tagHash != hashLocomotionTag)
+        {
+            yield return null;
+        }
+
+        handleInput = true;
     }
 }
